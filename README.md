870a21c6994e8fcc6e76165abd2415380d4a3278
ronnieoginga <oginga001ronney@gmail.com>
2018-12-11 10:06:32 +0300

    updated readme

--- README.md
+++ README.md
@@ -1,26 +1,32 @@
 #Quiz Board
 
 ##Description
+
 This is an application that helps the user with various kinds of questions.
 
 The user has to read the questions  which after answering it will give the score on clicking the submit button.
+
 ##Install
+
 $ apt install --global quizboard
 Usage
 $ quizboard --help
 
 ##Usage
-    $ quizboard [repo | user/repo]
+$ quizboard [repo | user/repo]
 
-  Examples
-    $ quizboard
-    $ quizboard myrepo
-    $ quizboard avajs/ava
+##Examples
+$ quizboard
+$ quizboard myrepo
+$ quizboard avajs/ava
 ##Tip
 Add alias gh=quizboard to your .zshrc/.bashrc, so that you can run it with $ gh instead.
 
 ##Related
+
 npm-home - Open the npm page of a package
 gh-upstream - Open the GitHub page of the upstream remote for the repo in the current directory
+
 ##License
+
 MIT © Ronnieoginga
--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,36 @@
-document.getElementById("form").onsubmit=function() {
-
-   1 = parseInt(document.$('input:radio[name = "1"]:checked').val);
-	 2 = parseInt(document.$('input:radio[name = "2"]:checked').val);
-	 3 = parseInt(document.$('input:radio[name = "3"]:checked').val);
-	 4 = parseInt(document.$('input:rradio[name = "4"]:checked').val);
 
 
-	 result = 1 + 2 + 3 + 4;
+$(document).ready(function(){
+  $("form #btn").click(function(){
+    var answer1 = $("input:radio[name=q1]:checked").val();
+    var answer2 = $("input:radio[name=q2]:checked").val();
+    var answer3 = $("input:radio[name=q3]:checked").val();
+    var answer4 = $("input:radio[name=q4]:checked").val();
 
-	document.getElementById("score").innerHTML = result;
+    alert(answer1);
 
-  return false;
+  });
+});
 
-}
-if (result == 0) {result2 = "I don't think you studied."};
-if (result == 25) {result2 = "You need to spend more time. Try again."};
-if (result == 50) {result2 = "I think you could do better. Try again."};
-if (result == 75) {result2 = "So close. Try again."};
-if (result == 100) {result2 = "Excellent! You're a JavaScript pro!"};
-document.getElementById("score2").innerHTML = result2;
+// document.getElementById("form").onsubmit=function() {
+//
+//    var 1 = parseInt(document.querySelector('input:radio[name = "q1"]:checked').val);
+// 	 var 2 = parseInt(document.querySelector('input:radio[name = "q2"]:checked').val);
+// 	 var 3 = parseInt(document.querySelector('input:radio[name = "q3"]:checked').val);
+// 	 var 4 = parseInt(document.querySelector('input:radio[name = "q4"]:checked').val);
+//
+//
+// 	 result = q1 + q2 + q3 + q4;
+//
+// 	document.getElementById("score").innerHTML = result;
+//
+//   return false;
+//
+//
+// if (result == 0) {result2 = "I don't think you studied."};
+// if (result == 25) {result2 = "You need to spend more time. Try again."};
+// if (result == 50) {result2 = "I think you could do better. Try again."};
+// if (result == 75) {result2 = "So close. Try again."};
+// if (result == 100) {result2 = "Excellent! You're a JavaScript pro!"};
+// document.getElementById("score2").innerHTML = result2;
+// }
