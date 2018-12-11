85e9c498db5bbef28d48af31a48aa2fd95456600
ronnieoginga <oginga001ronney@gmail.com>
2018-12-11 13:53:23 +0300

    deleted comments

--- js/scripts.js
+++ js/scripts.js
@@ -47,25 +47,4 @@ $(document).ready(function(){
   });
 });
 
-// document.getElementById("form").onsubmit=function() {
-//
-//    var 1 = parseInt(document.querySelector('input:radio[name = "q1"]:checked').val);
-// 	 var 2 = parseInt(document.querySelector('input:radio[name = "q2"]:checked').val);
-// 	 var 3 = parseInt(document.querySelector('input:radio[name = "q3"]:checked').val);
-// 	 var 4 = parseInt(document.querySelector('input:radio[name = "q4"]:checked').val);
-//
-//
-// 	 result = q1 + q2 + q3 + q4;
-//
-// 	document.getElementById("score").innerHTML = result;
-//
-//   return false;
-//
-//
-// if (result == 0) {result2 = "I don't think you studied."};
-// if (result == 25) {result2 = };
-// if (result == 50) {result2 = "I think you could do better. Try again."};
-// if (result == 75) {result2 = "So close. Try again."};
-// if (result == 100) {result2 = "Excellent! You're a JavaScript pro!"};
-// document.getElementById("score2").innerHTML = result2;
-// }
+ 
